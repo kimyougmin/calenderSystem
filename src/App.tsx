a2b5aa8306058e891aa700a448218206bc34c1b6
kym <dudals896@gmail.com>
2024-06-29 17:20:48 +0900

    add useContext CalenderContext.tsx
    1박 2일로 당일을 제외한 가장 빠른 날을 저장
    
    update App.tx
    
    코드 정리 및 불필요한 쿠키 이벤트 삭제
    useContext로 관리 렌더링 관리하고 쿠키는 새로 고침 시 값 저장용도

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import PaymentUi from "./components/PaymentUi";
+import {useCookies} from "react-cookie";
+import CalenderType from "./types/CalenderType";
+import CurrentDay from "./utils/CurrentDay";
+import TestModelReservedData from "./TextModel/TestModelReservedData";
+import CalenderObject from "./utils/CalenderObject";
+import NextMont from "./utils/NextMonth";
 
 function App() {
+    const [cookies, setCookies, removeCookie] = useCookies(['calenderDate']);
+    //예약 시스템 쿠키 관리
+    useEffect(() => {
+        if(cookies.calenderDate === undefined) {
+            setCookies('calenderDate',{
+                    isModal: false,
+                }
+            );
+        }
+        return () => {
+            if(cookies.calenderDate === undefined) return
+            setCookies('calenderDate', { isModal: cookies.calenderDate.isModal })
+        }
+    },[]);
+
     return (
-        <div className='app'>
+        <div className='app' >
             <div className='app-images'>
 
             </div>
@@ -12,7 +33,9 @@ function App() {
                 <div className='app-content'>
 
                 </div>
-                <PaymentUi />
+                <div className='app-payment'>
+                    <PaymentUi/>
+                </div>
             </div>
         </div>
     )
