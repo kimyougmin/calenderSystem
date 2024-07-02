cdfe3ff123897fc0aa0dd995c784a4a577a04a0d
kym <dudals896@gmail.com>
2024-07-03 04:53:11 +0900

    update PaymentUi.tsx Selector-day ui

--- src/components/CalenderModal.tsx
+++ src/components/CalenderModal.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function CalenderModal() {
 
     return (
-        <div className='calender-modal' style={{width:'80%', height:'20vh', backgroundColor:'black'}}>
+        <div className='calender-modal' style={{width:'80%', height:'20vh'}}>
 
         </div>
     );
--- src/components/PaymentUi.tsx
+++ src/components/PaymentUi.tsx
@@ -4,10 +4,11 @@ import CalenderModal from "./CalenderModal";
 import {calenderContext} from "../useContext/CalenderContext";
 import {Button} from "@mui/material";
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import CloseIcon from '@mui/icons-material/Close';
 
 function PaymentUi() {
     const {isModal, setIsModal, checkIn, checkOut, count} = React.useContext(calenderContext);
-    const modalRef = React.useRef<HTMLDivElement>(null);
+    const [isSelectDay, setIsSelectDay] = React.useState<boolean>(true);
     const [reservationInformation, setReservationInformation] = React.useState({
         dayPrice: 0,
         checkIn: checkIn,
@@ -27,33 +28,74 @@ function PaymentUi() {
             cleaningFee: 20000
         });
     };
-    const onClickHandler = (e: React.MouseEvent<HTMLDivElement>) => {
-        e.stopPropagation();
-        const refCurrent = modalRef.current as HTMLDivElement;
-        if (!refCurrent) {
-            return null;
-        }
-    }
-    const onOpenModal = () => {
+    const onOpenModalHandler = () => {
         setIsModal(true);
     }
+    const onSelectCheckInOutHandler = (e: React.MouseEvent<HTMLDivElement>) => {
+        const target = e.target as HTMLDivElement;
+        target.id === 'checkIn' ? setIsSelectDay(true) : setIsSelectDay(false)
+    }
     return (
-        <div className='payment-none' onClick={(e => onClickHandler(e))}>
+        <div className='payment-none'>
             <div className={'payment-header'}>
                 <p>₩{reservationInformation.dayPrice.toLocaleString()} </p>
                 <p>/박</p>
             </div>
             <div className={'payment-selector'}>
-                <div className={'selector-day'}>
-                    <div onClick={onOpenModal}>
-                        <p>check-in</p>
-                        <p>{checkIn[0]}. {checkIn[1]}. {checkIn[2]}.</p>
-                    </div>
-                    <div onClick={onOpenModal}>
-                        <p>check-out</p>
-                        <p>{checkOut[0]}. {checkOut[1]}. {checkOut[2]}.</p>
-                    </div>
-                </div>
+                {
+                    isModal ?
+                        isSelectDay ?
+                            <div className={'selector-checkIn'}>
+                                <div>
+                                    <div className='checkIn-btn' id={'checkIn'}
+                                         onClick={(e) => onSelectCheckInOutHandler(e)}>
+                                        <div id={'checkIn'}>
+                                            <p id={'checkIn'}>check-in</p>
+                                            <p id={'checkIn'}>{checkIn[0]}. {checkIn[1]}. {checkIn[2]}.</p>
+                                        </div>
+                                        <CloseIcon className={'closeIcon'}/>
+                                    </div>
+                                    <div className={'checkOut-btn'} id={'checkOut'}
+                                         onClick={(e) => onSelectCheckInOutHandler(e)}>
+                                        <div id={'checkOut'}>
+                                            <p id={'checkOut'}>check-out</p>
+                                            <p id={'checkOut'}>{checkOut[0]}. {checkOut[1]}. {checkOut[2]}.</p>
+                                        </div>
+                                        <CloseIcon className={'closeIcon'}/>
+                                    </div>
+                                </div>
+                            </div>
+                            :
+                            <div className={'selector-checkOut'} onClick={(e) => onSelectCheckInOutHandler(e)}>
+                                <div>
+                                    <div className='checkIn-btn' id={'checkIn'}>
+                                        <div id={'checkIn'}>
+                                            <p id={'checkIn'}>check-in</p>
+                                            <p id={'checkIn'}>{checkIn[0]}. {checkIn[1]}. {checkIn[2]}.</p>
+                                        </div>
+                                        <CloseIcon className={'closeIcon'}/>
+                                    </div>
+                                    <div className={'checkOut-btn'} id={'checkOut'}
+                                         onClick={(e) => onSelectCheckInOutHandler(e)}>
+                                        <div id={'checkOut'}>
+                                            <p id={'checkOut'}>check-out</p>
+                                            <p id={'checkOut'}>{checkOut[0]}. {checkOut[1]}. {checkOut[2]}.</p>
+                                        </div>
+                                        <CloseIcon className={'closeIcon'}/>
+                                    </div>
+                                </div>
+                            </div>
+                        : <div className={'selector-day'}>
+                            <div onClick={onOpenModalHandler}>
+                                <p>check-in</p>
+                                <p>{checkIn[0]}. {checkIn[1]}. {checkIn[2]}.</p>
+                            </div>
+                            <div onClick={onOpenModalHandler}>
+                                <p>check-out</p>
+                                <p>{checkOut[0]}. {checkOut[1]}. {checkOut[2]}.</p>
+                            </div>
+                        </div>
+                }
                 <div className={'selector-person'}>
                     <div>
                         <p>인원</p>
@@ -84,7 +126,7 @@ function PaymentUi() {
                 </span>
             </div>
             {isModal ?
-                <div ref={modalRef} className='modal-box'>
+                <div className='modal-box'>
                     <button onClick={() => {
                         setIsModal(false)
                     }}>close
