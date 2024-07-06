57ffbf4df7d26ffeae6c90c50f0e135d68b36842
kym <dudals896@gmail.com>
2024-07-06 14:47:13 +0900

    add type CalenderObjectType.ts, MonthDateType.ts

---
+++ src/TextModel/CalenderObjectType.ts
@@ -0,0 +1,5 @@
+export default interface CalenderObjectType {
+    year: number
+    month: number
+    reservation: number[]
+}
---
+++ src/types/MonthDateType.ts
@@ -0,0 +1,5 @@
+export default interface MonthDateType {
+    date: number
+    day: number
+    reservation: number
+}
